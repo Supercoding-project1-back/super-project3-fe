c2bc3f845e21c5c96141272362bd1255161c7d93
cookie0215 <jina_0215@naver.com>
2024-08-04 23:10:16 +0900

    fix: inex.js에 font.scss, variables.scss, reset.scss 경로 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import './styles/fonts.scss';
+import './styles/variables.scss';
+import './styles/reset.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
