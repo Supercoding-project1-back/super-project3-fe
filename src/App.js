a08d92c7bfebc66a125f21827b2d2d19a67e6f39
cookie0215 <jina_0215@naver.com>
2024-08-05 21:29:57 +0900

    rename: Nav.jsx 파일명 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import Nav from "./components/nav/nav";
 
 function App() {
   return (
     <div>
       <Header />
+      <Nav />
     </div>
   );
 }
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const nav = () => {
+const Nav = () => {
   return (
     <div>
 
@@ -8,4 +8,4 @@ const nav = () => {
   );
 };
 
-export default nav;
+export default Nav;
