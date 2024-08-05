80773ebd1ebd9bbd93e892e697940dbec345b493
cookie0215 <jina_0215@naver.com>
2024-08-05 22:59:02 +0900

    feat: AppRoutes 페이지 설정
    - AppRoutes 생성 및 페이지 연결

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,7 +4,10 @@ import Layout from '../components/layout/Layout';
 import Main from '../pages/main/Main';
 import Login from '../pages/login/Login';
 import SignUp from '../pages/sign-up/SignUp';
-
+import PostDetail from '../pages/post-detail/PostDetail';
+import WritePost from '../pages/write-post/WritePost';
+import ChatPage from '../pages/chat-page/ChatPage';
+import MyPage from '../pages/my-page/MyPage';
 
 const AppRoutes = createBrowserRouter([
   {
@@ -14,6 +17,10 @@ const AppRoutes = createBrowserRouter([
       { index: true, element: <Main /> },
       { path: "login", element: <Login /> },
       { path: "signup", element: <SignUp /> },
+      { path: "post-detail", element: <PostDetail /> },
+      { path: "write-post", element: <WritePost /> },
+      { path: "chat", element: <ChatPage /> },
+      { path: "mypage", element: <MyPage /> },
     ],
   }
 ]);
