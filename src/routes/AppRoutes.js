91661d15e70b256f4de270baa01ca9afc66b9bbc
ggoldJeongg <manage06@naver.com>
2024-08-09 00:12:21 +0900

    chore: 라우터 처리 변경
    - 상세페이지와 게시글작성페이지의 경로 처리 변경
    - my-page에서 profile로 경로명 변경

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
-import Layout from '../components/layout/Layout';
-import Main from '../pages/main/Main';
-import Login from '../pages/login/Login';
-import SignUp from '../pages/sign-up/SignUp';
-import PostDetail from '../pages/post-detail/PostDetail';
-import WritePost from '../pages/write-post/WritePost';
-import ChatPage from '../pages/chat-page/ChatPage';
-import MyPage from '../pages/my-page/MyPage';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+import Main from "../pages/main/Main";
+import Login from "../pages/login/Login";
+import SignUp from "../pages/sign-up/SignUp";
+import PostDetail from "../pages/post-detail/PostDetail";
+import WritePost from "../pages/write-post/WritePost";
+import ChatPage from "../pages/chat/Chat";
+import Profile from "../pages/profile/Profile";
 
 const AppRoutes = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       { index: true, element: <Main /> },
       { path: "login", element: <Login /> },
       { path: "signup", element: <SignUp /> },
-      { path: "post-detail", element: <PostDetail /> },
-      { path: "write-post", element: <WritePost /> },
+      { path: "post/:id", element: <PostDetail /> },
+      { path: "post/new", element: <WritePost /> },
       { path: "chat", element: <ChatPage /> },
-      { path: "mypage", element: <MyPage /> },
+      { path: "profile", element: <Profile /> },
     ],
-  }
+  },
 ]);
 
 export default AppRoutes;
