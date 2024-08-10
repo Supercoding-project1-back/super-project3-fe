93f451cdb398844aa61de123a95822a03096a511
cookie0215 <jina_0215@naver.com>
2024-08-10 19:37:27 +0900

    feat: Header & Nav 페이지별에 따른 UI 구현 기능 외
    1. 각 컴포넌트에 index.js 추가
    - components/layout
    - components/header
    - components/nav
    - pages/auth
    - pages/chat
    - pages/chat/chat-detail
    
    2. AppRoutes에서 경로 수정
    - components/layout
    - pages/auth
    - pages/chat
    - pages/chat/chat-detail
    
    3. Nav 컴포넌트 페이지 경로 조건에 따라 null 값 설정
    - 게시글상세페이지 & 글쓰기 페이지는 nav 없음
    
    4. Header 컴포넌트 각 페이지별로 UI 형태 다르게 구현
    - 기본 헤더 : 로고+검색
    - 메인페이지 헤더 : 위치+검색
    - 게시글상세페이지 헤더 : 뒤로가기+(더보기)
    - 글쓰기페이지 헤더 : 뒤로가기+등록
    - 채팅방페이지 : 뒤로가기+닉네임

--- src/components/core/index.js
+++ src/components/core/index.js
@@ -1,1 +1,1 @@
-export { default as Icon } from "./icon-feild/IconField";
+export { default as Icon } from "./icon-field/IconField";
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,131 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import IconField from '../core/icon-field/IconField';
+import { useNavigate } from 'react-router';
 
-const Header = () => {
-  return (
-    <header>
-      <div>
-        <IconField
-          type={'iconLogo'}
-          className={`${styles.icon} ${styles.iconLogo}`}
-        />
-      </div>
-      <div>
-        <IconField
-          type={'iconSearch'}
-          className={`${styles.icon} ${styles.iconSearch}`}
-        />
-      </div>
-    </header>
-  );
+const Header = ({ type, location }) => {
+  const navigate = useNavigate();
+  console.log(window.history);
+
+  const handleClickBack = () => {
+    if (window.history.length > 2) {
+      navigate(-1);
+    } else {
+      navigate('/');
+    }
+  }
+
+  const headerContent = () => {
+    switch (type) {
+      case 'main':
+        // 메인페이지 헤더 : 위치+검색
+        return (
+          <header className={`${styles.header} ${styles.mainHeader}`}>
+            <div className={styles.leftWrap}>
+              <span>{location}</span>
+              <IconField
+                type={'iconLocation'}
+                className={`${styles.icon} ${styles.iconLocation}`}
+              />
+            </div>
+            <div className={styles.rightWrap}>
+              <IconField
+                type={'iconSearch'}
+                className={`${styles.icon} ${styles.iconSearch}`}
+              />
+            </div>
+          </header>
+        );
+        break;
+
+      // 게시글상세페이지 헤더 : 뒤로가기+(더보기-user에따라)
+      case 'detail':
+        return (
+          <header className={`${styles.header} ${styles.detailHeader}`}>
+            <div
+              className={styles.leftWrap}
+              onClick={handleClickBack}
+            >
+              <IconField
+                type={'iconBack'}
+                className={`${styles.icon} ${styles.iconBack}`}
+              />
+            </div>
+            <div className={styles.rightWrap}>
+              <IconField
+                type={'iconMore'}
+                className={`${styles.icon} ${styles.iconMore}`}
+              />
+            </div>
+          </header>
+        );
+        break;
+
+      // 글쓰기페이지 헤더 : 뒤로가기+등록
+      case 'write':
+        return (
+          <header className={`${styles.header} ${styles.writeHeader}`}>
+            <div
+              className={styles.leftWrap}
+              onClick={handleClickBack}
+            >
+              <IconField
+                type={'iconBack'}
+                className={`${styles.icon} ${styles.iconBack}`}
+              />
+            </div>
+            <div className={styles.rightWrap}>
+              <button>등록</button>
+            </div>
+          </header>
+        );
+        break;
+
+      // 채팅방페이지 : 뒤로가기+닉네임
+      case 'chat':
+        return (
+          <header className={`${styles.header} ${styles.chatHeader}`}>
+            <div
+              className={styles.leftWrap}
+              onClick={handleClickBack}
+            >
+              <IconField
+                type={'iconBack'}
+                className={`${styles.icon} ${styles.iconBack}`}
+              />
+            </div>
+            <div>
+              <span>상대방 닉네임1</span>
+            </div>
+            <div className={styles.rightWrap}>
+              {null}
+            </div>
+          </header>
+        );
+        break;
+
+      default:
+        // 기본 헤더 : 로고+검색
+        return (
+          <header className={styles.header}>
+            <div className={styles.leftWrap}>
+              <IconField
+                type={'iconLogo'}
+                className={`${styles.icon} ${styles.iconLogo}`}
+              />
+            </div>
+            <div className={styles.rightWrap}>
+              <IconField
+                type={'iconSearch'}
+                className={`${styles.icon} ${styles.iconSearch}`}
+              />
+            </div>
+          </header>
+        )
+    }
+  }
+
+  return <>{headerContent()}</>;
 }
 
 export default Header;
