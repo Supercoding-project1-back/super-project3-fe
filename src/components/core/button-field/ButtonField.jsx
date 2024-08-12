5ae9a261f9ad517b9e7c8334a37e8647ec270be9
ggoldJeongg <manage06@naver.com>
2024-08-13 01:02:08 +0900

    feat: className 추가
    - className: 클래스명을 따로 지정하여 스타일링 가능하도록 추가

--- src/components/core/button-field/ButtonField.jsx
+++ src/components/core/button-field/ButtonField.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./ButtonField.module.scss";
 
-const Button = ({ label, onClick, isActive, type }) => {
+const Button = ({ label, onClick, isActive, type, className }) => {
   const buttonClass = isActive ? styles.active : styles.inactive;
 
   return (
     <button
-      className={`${styles.button} ${buttonClass}`}
+      className={`${styles.button} ${buttonClass} ${className}`}
       onClick={() => onClick(type)}
     >
       {label}
