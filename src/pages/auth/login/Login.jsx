c2497505204a7b06ac8637693ed28f66ce78a696
supercoding_backend <myec1235@naver.com>
2024-08-15 15:40:17 +0900

    feat: 로그인 첫 페이지 이미지 추가

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
+import { ReactComponent as ChatIcon } from "../../../assets/imgs/homeLogo.svg";
+import styles from "./Login.module.scss";
 
 const Login = () => {
   return (
-    <div>
-      로그인 입니다.
-    </div>
+    <>
+      <div className={styles.container}>
+        <ChatIcon className={styles["svg-icon"]} />
+      </div>
+      <div>
+        <h1>지금 시작하세요</h1>
+      </div>
+    </>
   );
 };
 
