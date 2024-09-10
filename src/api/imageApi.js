d197f5530b28f717436cb62f84f0749d07a8d91c
cookie0215 <jina_0215@naver.com>
2024-09-10 19:08:21 +0900

    chore: 토큰 수정

--- src/api/imageApi.js
+++ src/api/imageApi.js
@@ -5,7 +5,6 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 export const uploadPostDetail = async (postId, formData) => {
   try {
     const token = localStorage.getItem('token');
-
     const response = await axios.post(
       `${API_BASE_URL}/api/posts/create-postDetail/${postId}`,
       formData,
