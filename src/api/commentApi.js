adf03a3be01b6e69eb76ddaa901f23433ef36139
cookie0215 <jina_0215@naver.com>
2024-09-24 01:03:10 +0900

    refactor: api 코드 일관성 및 이중 슬래쉬 문제 해결

--- src/api/commentApi.js
+++ src/api/commentApi.js
@@ -1,36 +1,37 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
+// API 기본 설정
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
 
 // 댓글 리스트
 export const getCommentList = async (postId, page, size) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/api/comments/comments/${postId}`, {
+    const response = await api.get(`/api/comments/comments/${postId}`, {
       params: {
         page,
         size,
         sort: [],
-      }
+      },
     });
     return response.data;
   } catch (error) {
     console.error('댓글 리스트 API 오류 발생:', error);
   }
-}
-
-
+};
 
 // 댓글 등록 API
 export const createComment = async (postId, content) => {
   try {
-    const token = localStorage.getItem('token');
-    const response = await axios.post(
-      `${API_BASE_URL}/api/comments/create-comment`,
+    const response = await api.post(
+      "/api/comments/create-comment",
       { postId, content },
       {
         headers: {
-          Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
       }
@@ -40,25 +41,21 @@ export const createComment = async (postId, content) => {
     console.error('댓글 등록 API 오류 발생:', error);
     throw error;
   }
-}
-
+};
 
 // 댓글 수정 API
 export const modifyComment = async (commentId, postId, newContent) => {
   try {
-    const token = localStorage.getItem('token');
-
     // 요청 전 데이터 확인
     console.log('수정할 댓글 ID:', commentId);
     console.log('수정할 포스트 ID:', postId);
     console.log('수정할 댓글 내용:', newContent);
 
-    const response = await axios.put(
-      `${API_BASE_URL}/api/comments/modify-comment/${commentId}`,
+    const response = await api.put(
+      `/api/comments/modify-comment/${commentId}`,
       { postId: postId, content: newContent },
       {
         headers: {
-          Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
       }
@@ -72,18 +69,10 @@ export const modifyComment = async (commentId, postId, newContent) => {
   }
 };
 
-
 // 댓글 삭제 API
 export const deleteCommentApi = async (commentId) => {
   try {
-    const token = localStorage.getItem('token');
-    const response = await axios.delete(
-      `${API_BASE_URL}/api/comments/delete-comment/${commentId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+    const response = await api.delete(`/api/comments/delete-comment/${commentId}`);
     return response.data;
   } catch (error) {
     console.error("댓글 삭제 API 오류 발생:", error);
